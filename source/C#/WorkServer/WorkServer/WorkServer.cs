5627140457cf20f5ff7cbdad6a41db65aaa8d506
nowonbun@naver.com <ghkdtnsduq1>
2017-02-21 02:21:19 +0900

    Source commit;

---
+++ source/C#/WorkServer/WorkServer/WorkServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkServer
+{
+    abstract class WorkServer
+    {
+        private Client client = null;
+        public WorkServer(Client client)
+        {
+            this.client = client;
+        }
+        protected Client ClientSocket
+        {
+            get { return this.client; }
+        }
+        public abstract bool Initialize(HandShake header);
+        public abstract void Run();
+    }
+}
