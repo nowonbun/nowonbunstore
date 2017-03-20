f0f2648bb7ba8281edc9d73b07c28c669e27e837
nowonbun@naver.com <ghkdtnsduq1>
2017-03-20 23:11:01 +0900

    Refactoring...

--- source/C#/WebScraping/WebScraping/Program.cs
+++ source/C#/WebScraping/WebScraping/Program.cs
@@ -10,6 +10,27 @@ namespace WebScraping
     {
         static void Main(string[] args)
         {
+            String asdf = "TEST[1123]";
+            Console.WriteLine(GetSplitKey(asdf));
+            Console.ReadKey();
+
+        }
+        private static int GetSplitKey(String key)
+        {
+            int spos = key.IndexOf("[")+1;
+            int epos = key.LastIndexOf("]");
+            if (spos < 0 || epos < 0)
+            {
+                throw new Exception("xPath 가 맞지 않습니다.");
+            }
+            try
+            {
+                return Convert.ToInt32(key.Substring(spos, epos - spos));
+            }
+            catch (FormatException)
+            {
+                throw new FormatException("xPath 가 맞지 않습니다.");
+            }
         }
     }
 }
