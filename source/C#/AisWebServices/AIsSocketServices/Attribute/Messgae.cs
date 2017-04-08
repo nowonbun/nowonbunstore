d2cd5149570523f20a8771f0e19b9f6bd71c01b3
nowonbun@naver.com <ghkdtnsduq1>
2017-04-09 02:24:58 +0900

    database access system

---
+++ source/C#/AisWebServices/AIsSocketServices/Attribute/Messgae.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AIsSocketServices
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class Messgae : System.Attribute
+    {
+        public String KeyName
+        {
+            get;
+            set;
+        }
+        public Messgae(String keyName)
+        {
+            this.KeyName = keyName;
+        }
+    }
+}
---
+++ source/C#/AisWebServices/AisWebServices/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using AIsSocketServices;
+using AisWebServicesDao;
+
+namespace AisWebServices
+{
+
+    public class AisApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            FactoryDao.CreateInstance(System.Configuration.ConfigurationManager.ConnectionStrings["conn"].ConnectionString);
+            AisSocketServices.GetInstance().Run();
+        }
+    }
+}
