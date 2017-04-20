5bcbbe84816656367f309f47073048a3b5688883
nowonbun <nowonbun@naver.com>
2017-04-21 00:25:43 +0900

    Add RefreshRouter

---
+++ source/C#/RefreshRouter/RefreshRouter/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gecko;
+using Gecko.DOM;
+using System.IO;
+
+namespace RefreshRouter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Xpcom.EnableProfileMonitoring = false;
+            var app_dir = Path.GetDirectoryName(Application.ExecutablePath);
+            Xpcom.Initialize(Path.Combine(app_dir, "Firefox"));
+            InitializeComponent();
+            webBrowser1.DocumentCompleted += webBrowser1_DocumentCompleted;
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            webBrowser1.Navigate("http://192.168.0.7");
+        }
+
+        void webBrowser1_DocumentCompleted(object sender, Gecko.Events.GeckoDocumentCompletedEventArgs e)
+        {
+            if (e.Uri == null)
+            {
+                return;
+            }
+            if (e.Uri.ToString().IndexOf("login.html") != -1)
+            {
+                GeckoDocument doc = webBrowser1.Document;
+                /*GeckoInputElement element = doc.GetElementsByName("nosave_Username").Single() as GeckoInputElement;
+                element.Value = "admin";*/
+                GeckoInputElement element = doc.GetElementById("id_nosave_Password") as GeckoInputElement;
+                if (element != null)
+                {
+                    element.Value = "password";
+                    element = doc.GetElementById("id_login") as GeckoInputElement;
+                    element.Click();
+                }
+            }
+            if (e.Uri.ToString().IndexOf("index_pc.html") != -1)
+            {
+                webBrowser1.Navigate("http://192.168.0.7/init.html");
+            }
+            if (e.Uri.ToString().IndexOf("init.html") != -1)
+            {
+                GeckoDocument doc = webBrowser1.Document;
+                GeckoInputElement element = doc.GetElementById("id_rebootBtn") as GeckoInputElement;
+                if (element != null)
+                {
+                    element.Click();
+                    Timer timer = new Timer();
+                    timer.Interval = 1000 * 60;
+                    timer.Tick += (s,a) =>
+                    {
+                        this.Close();
+                    };
+                    timer.Enabled = true;
+                }
+            }
+
+        }
+    }
+}
---
+++ source/C#/RefreshRouter/RefreshRouter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefreshRouter
+{
+    static class Program
+    {
+        /// <summary>
+        /// 해당 응용 프로그램의 주 진입점입니다.
+        /// </summary>
+        [STAThread]
+        static void Main(String[] args)
+        {
+            PingChecker checker = new PingChecker();
+
+            if (!checker.Run() || (args.Length > 0 && args[0] == "0"))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+        }
+    }
+}
