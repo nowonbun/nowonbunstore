79c9d5c4ac5d55d12e4becd98eb73efeb2114c54
nowonbun <nowonbun@naver.com>
2017-05-17 01:32:09 +0900

    Test Project refresh!

--- source/C#/DataChecker/DataChecker/Program.cs
+++ source/C#/DataChecker/DataChecker/Program.cs
@@ -7,12 +7,13 @@ using System.Net;
 using System.Net.Sockets;
 using System.Web;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace DataChecker
 {
     class Program
     {
-        
+
         public enum HttpMethod
         {
             POST,
@@ -20,31 +21,53 @@ namespace DataChecker
         }
         static void Main(string[] args)
         {
-            DateTime a = new DateTime(1490414594000);
-            a = new DateTime(2017, 04, 25);
-            Console.WriteLine(a.Ticks.ToString());
-            Console.ReadKey();
-            return;
+            //1493046000000
+            //1490414594000
+            /*Int64 ticks = 1490414594000;
+            ticks *= 10000L;
+            //ticks += 621355968000000000L;
+            ticks += 63628076000000000L;
+            DateTime a = new DateTime(ticks);
+            DateTime b = new DateTime(2017, 04, 25);
+            DateTime c = new DateTime(621355968000000000L);
+            c = c.AddHours(9);
+            Console.WriteLine((b.Ticks - a.Ticks).ToString());
+            Console.ReadKey();*/
+
             //String key = GetWeb("http://localhost:8080/HouseholdData/UserCheck", "GID=103311668963248489400&NAME=黄淳皣&EMAIL=nowonbun@gmail.com");
             //String key = GetWeb("http://localhost:8080/HouseholdData/GetHouseholdList", "GID=103311668963248489400&YEAR=2016&MONTH=2");
             //String key = GetWeb("http://192.168.0.2:8080/HouseholdData/CheckUser", "GID=103311668963248489401");
-            String key = GetWeb("http://localhost:8080/HouseholdData/GetHouseholdList2", "GID=103311668963248489400&YEAR=2016&MONTH=2&CATEGORY=020");
+            //String key = GetWeb("http://localhost:8080/HouseholdData/GetHouseholdList2", "GID=103311668963248489400&YEAR=2016&MONTH=2&CATEGORY=020");
+            var tt = new Dictionary<String, String>() { { "gid", "test" } };
+            String json = JsonConvert.SerializeObject(tt);
+            var base64 = System.Convert.ToBase64String(Encoding.UTF8.GetBytes(json));
+            
+            String key = GetWeb("http://localhost/Household/GetMaster.php","p=SY" + base64);
+            key = key.Substring(2);
+            byte[] data = System.Convert.FromBase64String(key);
+            key = Encoding.UTF8.GetString(data);
             Console.WriteLine(key);
-            String data = GetData(key);
-            Console.WriteLine(data);
 
-            Console.ReadKey();
+            Dictionary<String, Object> ret = JsonConvert.DeserializeObject<Dictionary<String, Object>>(key);
+            Console.WriteLine(key);
+            //String data = GetData(key);
+            //Console.WriteLine(data);
+            
+            Console.ReadLine();
         }
-        public static string GetWeb(String url, String param)
+        public static string GetWeb(String url, String param = null)
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
             request.Method = "POST";
             request.ContentType = "application/x-www-form-urlencoded";
-            byte[] byteArray = Encoding.UTF8.GetBytes(param);
-            request.ContentLength = byteArray.Length;
-            using (Stream dataStream = request.GetRequestStream())
+            if (param != null)
             {
-                dataStream.Write(byteArray, 0, byteArray.Length);
+                byte[] byteArray = Encoding.UTF8.GetBytes(param);
+                request.ContentLength = byteArray.Length;
+                using (Stream dataStream = request.GetRequestStream())
+                {
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+                }
             }
 
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
