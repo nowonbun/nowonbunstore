5b5acf90b4a2b0082c9816374b15ba424432f42f
SoonYub <nowonbun@naver.com>
2018-04-10 22:15:15 +0900

    The source was committed for the default design of window form

---
+++ source/C#/WindowExample/WindowExample/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            ControlFactory.SetForm(this);
+            QueueThread.Start();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            AddCheckBoxColumn();
+            AddColumn(100, "Value1");
+            AddColumn(100, "Value2");
+            AddColumn(100, "Value3");
+            AddColumn(100, "Value4");
+            AddColumn(100, "Value5");
+            //dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            //dataGridView1.ColumnHeadersHeight = 30;
+            //dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            //dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            base.OnSizeChanged(e);
+            this.dataGridView1.Height = this.Size.Height - 120;
+            OnStatus.Visible = false;
+            OffStatus.Visible = true;
+            toolStripProgressBar1.Visible = false;
+            toolStripStatusLabel1.Visible = false;
+            toolStripStatusLabel1.Text = "";
+        }
+
+        public void SetStatusMessage(String msg)
+        {
+            QueueThread.Push(() =>
+            {
+                QueueThread.InvokeControl(statusStrip1, () =>
+                {
+                    OnStatus.Visible = true;
+                    OffStatus.Visible = false;
+                    toolStripProgressBar1.Visible = true;
+                    toolStripStatusLabel1.Visible = true;
+                });
+            });
+        }
+        public void HideStatusMessage()
+        {
+            QueueThread.Push(() =>
+            {
+                QueueThread.InvokeControl(statusStrip1, () =>
+                {
+                    OnStatus.Visible = false;
+                    OffStatus.Visible = true;
+                    toolStripProgressBar1.Visible = false;
+                    toolStripStatusLabel1.Visible = false;
+                    toolStripStatusLabel1.Text = "";
+                });
+            });
+        }
+
+        private void AddCheckBoxColumn()
+        {
+            DataGridViewCheckBoxColumn column = new DataGridViewCheckBoxColumn();
+            column.Width = 30;
+            column.FalseValue = false;
+            column.TrueValue = true;
+            var allcheck = new CheckBox();
+            allcheck.Size = new Size(15, 15);
+            //location
+            allcheck.Location = new Point(50, 10);
+            allcheck.Click += (s, e) =>
+            {
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    var c = dataGridView1.Rows[i].Cells[0] as DataGridViewCheckBoxCell;
+                    c.Value = allcheck.Checked;
+                }
+            };
+            this.dataGridView1.Controls.Add(allcheck);
+            dataGridView1.Columns.Add(column);
+
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            dataGridView1.ColumnHeadersHeight = 30;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+        }
+        private void AddColumn(int width, String name, bool _readonly = false, Type type = null)
+        {
+            if (type == null)
+            {
+                type = typeof(String);
+            }
+            DataGridViewColumn column = new DataGridViewColumn();
+            column.Width = width;
+            column.ValueType = type;
+            column.HeaderText = name;
+            column.CellTemplate = new DataGridViewTextBoxCell();
+            column.ReadOnly = _readonly;
+            dataGridView1.Columns.Add(column);
+        }
+        public DataGridView GetGridData()
+        {
+            return dataGridView1;
+        }
+    }
+}
---
+++ source/C#/WindowExample/WindowExample/UserControl1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NPOI.SS.UserModel;
+using NPOI.SS.Util;
+using NPOI.XSSF.UserModel;
+using NPOI.XSSF.Model;
+using System.IO;
+using Ionic.Zip;
+
+namespace WindowExample
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            ControlFactory.SetControl(this);
+            InitializeComponent();
+        }
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            button1.Click += Button1_Click;
+        }
+        private void EnableControl(bool enable)
+        {
+            QueueThread.InvokeControl(this, () =>
+            {
+                button1.Enabled = enable;
+                button2.Enabled = enable;
+            });
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            QueueThread.Push(() =>
+            {
+                EnableControl(false);
+                ControlFactory.GetForm<Form1>().SetStatusMessage("The excel is started to create.");
+                DataGridView grid = ControlFactory.GetForm<Form1>().GetGridData();
+                QueueThread.InvokeControl(grid, () =>
+                {
+                    grid.Enabled = false;
+                });
+                //Create workbook
+                IWorkbook book = new XSSFWorkbook();
+                //Create font
+                IFont boldFont = book.CreateFont();
+                boldFont.Boldweight = (short)FontBoldWeight.Bold;
+                ICellStyle boldStyle = book.CreateCellStyle();
+                boldStyle.SetFont(boldFont);
+                boldStyle.BorderBottom = NPOI.SS.UserModel.BorderStyle.Medium;
+
+                int rowidx = 0;
+                //Create sheet;
+                ISheet sheet = book.CreateSheet("TEST");
+                //Header
+                IRow row = sheet.CreateRow(rowidx++);
+                for (int j = 1; j < grid.ColumnCount; j++)
+                {
+                    String data = grid.Columns[j].HeaderText;
+                    ICell cell = row.CreateCell(j);
+                    cell.SetCellValue(data);
+                    cell.CellStyle = boldStyle;
+                }
+
+                //Contents
+                for (int i = 0; i < grid.RowCount; i++)
+                {
+                    DataGridViewRow gridrow = grid.Rows[i];
+                    DataGridViewCheckBoxCell checkcehll = gridrow.Cells[0] as DataGridViewCheckBoxCell;
+                    if (checkcehll.Value == null || !((bool)checkcehll.Value))
+                    {
+                        continue;
+                    }
+                    row = sheet.CreateRow(rowidx++);
+                    for (int j = 1; j < grid.ColumnCount; j++)
+                    {
+                        String data = grid[j, i].Value.ToString();
+                        ICell cell = row.CreateCell(j);
+                        cell.SetCellValue(data);
+                    }
+                }
+
+                ControlFactory.GetForm<Form1>().SetStatusMessage("The excel is be creating.");
+                //Create excel
+                using (FileStream stream = new FileStream(Path.GetDirectoryName(Application.ExecutablePath) + "\\TEST.xlsx", FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    book.Write(stream);
+                }
+
+                ControlFactory.GetForm<Form1>().SetStatusMessage("The excel is be compressing.");
+                //compress zip
+                using (ZipFile zip = new ZipFile())
+                {
+                    for (int i = 0; i < 1; i++)
+                    {
+                        using (MemoryStream memory = new MemoryStream())
+                        {
+                            book.Write(memory);
+                            byte[] data = memory.ToArray();
+                            zip.AddEntry("Test" + i + ".xlsx", data);
+                        }
+                    }
+
+                    zip.Save(Path.GetDirectoryName(Application.ExecutablePath) + "\\TEST.zip");
+                }
+
+                ControlFactory.GetForm<Form1>().HideStatusMessage();
+                QueueThread.InvokeControl(grid, () =>
+                {
+                    grid.Enabled = true;
+                });
+                EnableControl(true);
+            });
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            QueueThread.Push(() =>
+            {
+                EnableControl(false);
+                ControlFactory.GetForm<Form1>().SetStatusMessage("The data is started to insert to the grid.");
+                DataGridView grid = ControlFactory.GetForm<Form1>().GetGridData();
+                QueueThread.InvokeControl(grid, () =>
+                {
+                    grid.Enabled = false;
+                });
+                for (int i = 0; i < 10000; i++)
+                {
+                    QueueThread.InvokeControl(grid, () =>
+                    {
+                        grid.Rows.Add(true, "TEST1_" + i, "TEST2_" + i, "TEST3_" + i, "TEST4_" + i, "TEST5_" + i);
+                    });
+                }
+                QueueThread.InvokeControl(grid, () =>
+                {
+                    grid.Enabled = true;
+                });
+                ControlFactory.GetForm<Form1>().HideStatusMessage();
+                EnableControl(true);
+            });
+
+        }
+    }
+}
